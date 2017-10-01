bad9011b292eaa2069bcdbca5a45bdacf29511ec
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-10-01 10:43:16 -0700

    added messages controller and route

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,2 +1,10 @@
 class Api::MessagesController < ApplicationController
+  def index
+    messages = Message.all
+    if messages
+      render json: messages, status: 200
+    else
+      render json: {message: "there are no messages"}
+    end
+  end
 end
