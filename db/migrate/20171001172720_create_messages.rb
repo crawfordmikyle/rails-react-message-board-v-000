769dce378aef311a32e9a925716a9f6ee11fd8ef
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-10-01 10:34:14 -0700

    added migrations

--- db/migrate/20171001172720_create_messages.rb
+++ db/migrate/20171001172720_create_messages.rb
@@ -1,7 +1,9 @@
 class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
-
+      t.string :title
+      t.string :message_content
+      t.integer :user_id
       t.timestamps
     end
   end
