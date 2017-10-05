a98cccedd7a55a8c336d410a3a99303d74bea136
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-10-05 13:10:59 -0700

    import rout and switch into message

--- client/src/Components/Message.js
+++ client/src/Components/Message.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+import {Link, Route, Switch} from 'react-router-dom'
 
 const Message = ({message,match}) => {
   return(
