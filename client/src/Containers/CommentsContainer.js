6617972998265eaa434b790a7c4d91484002742a
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-10-05 09:26:25 -0700

    working on comments front-end

---
+++ client/src/Containers/CommentsContainer.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import {connect} from 'react-redux '
+
+class CommentsContainer extends Component {
+  constructor(){
+
+  }
+}
+
+export default CommentsContainer
