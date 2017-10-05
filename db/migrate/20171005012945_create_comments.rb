a6033f6fd6407f270af6f73f53dcc2df07cda6ed
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-10-04 18:49:09 -0700

    changed string to text  for content
    changed string to text  for content and connected messages and comments

--- db/migrate/20171005012945_create_comments.rb
+++ db/migrate/20171005012945_create_comments.rb
@@ -1,7 +1,8 @@
 class CreateComments < ActiveRecord::Migration[5.1]
   def change
     create_table :comments do |t|
-
+      t.text :comment_content
+      t.integer :message_id
       t.timestamps
     end
   end
