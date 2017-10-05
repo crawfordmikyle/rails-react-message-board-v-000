89a2f57195afe0a7ce83d4c6ef8a3ad2a1426e98
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-10-05 13:09:26 -0700

    added comment migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171001172720) do
+ActiveRecord::Schema.define(version: 20171005200656) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "message_id"
+    t.string "comment_content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
