c160d0f6eb22ae0bc7e90c515892dd6ed1ac176f
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-10-05 13:27:49 -0700

    working on comments routes

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,6 @@
+require 'pry'
 class Api::CommentsController < ApplicationController
+  def show
+    binding.pry
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :messages
   end
+
+  get '/api/messages/:id/comments', to: 'api/comments#show'
+
 end
